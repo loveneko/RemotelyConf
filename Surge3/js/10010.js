789c1760a9134f325ee58883bf59f8588824202b
madoka xiaoyuan <45760201+madokaxiaoyuan@users.noreply.github.com>
2019-10-28 16:36:58 +0800

    Create 10010.js

---
+++ Surge3/js/10010.js
@@ -0,0 +1,11 @@
+let phone =1181357502;//填入电话号码
+let url = "https://mina.10010.com/wxapplet/bind/getIndexData/alipay/alipaymini?user_id="+phone;
+$httpClient.get(url,function (error, response, data) {
+    if(!error){
+        let dataobj = JSON.parse(data);
+        var result = dataobj.dataList;
+        $notification.post(dataobj.packageName+'   时间:'+dataobj.flush_date_time,result[1].remainTitle+result[1].number+result[1].unit,result[0].remainTitle+result[0].number+result[0].unit+' '+result[2].remainTitle+result[2].number+result[2].unit,data);
+
+    }
+});
+$done();
